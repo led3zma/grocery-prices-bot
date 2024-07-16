ab4cf45d4436dac801ae297111a957785a0c43d3
Alejandro Salas Ledezma <asalasledezma16@gmail.com>
2024-07-16 05:46:38 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import logging
+import os
+
+from db import Base, engine
+from handlers import start
+
+from dotenv import load_dotenv
+from telegram.ext import ApplicationBuilder, CommandHandler
+
+load_dotenv()
+
+Base.metadata.create_all(bind=engine)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(os.environ.get('BOT_TOKEN')).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
