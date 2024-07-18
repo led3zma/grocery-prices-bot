ffd2336ccb59f8a9e650e6a517481d9d6a07524c
Alejandro Ledezma <38331371+led3zma@users.noreply.github.com>
2024-07-18 05:42:18 -0400

    Implemented db models (#1)
    * Added models
    
    * Changed declarative base class to use orm.DeclarativeBase

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./database.sqlite"
 
@@ -9,7 +8,9 @@ engine = create_engine(
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
 
 
 def get_db():
