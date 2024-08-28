cdf6363671389bff3d519ed5176b30bd1af6f7e8
Alejandro Ledezma <asalasledezma16@gmail.com>
2024-08-28 16:35:12 -0400

    Implemented conversation handlers for registring a product

--- handlers.py
+++ handlers.py
@@ -1,6 +1,10 @@
+from enum import Enum
+
+import conversation_states
+
 from telegram import Update
 from telegram.constants import ParseMode
-from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes, ConversationHandler
 
 MENU = '*Opciones*\n/listar\n/registrar\n/buscar'
 
@@ -22,9 +26,40 @@ async def list_products(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(update.effective_chat.id, '*Listing...(WIP)*', parse_mode=ParseMode.MARKDOWN)
 
 
-async def register_products(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def register_products(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     await context.bot.send_message(update.effective_chat.id, '*Introduce la descripción del producto...(WIP)*', parse_mode=ParseMode.MARKDOWN)
+    return conversation_states.REGISTER_NAME
+
+
+async def register_product_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data['new_product_name'] = update.message.text.upper()
+    await update.message.reply_text('Ingresa el precio para el producto...')
+    return conversation_states.REGISTER_PRICE
+
+
+async def register_product_price(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data['new_product_price'] = update.message.text.upper()
+    await update.message.reply_text('¿En qué tienda viste este producto a este precio?')
+    return conversation_states.REGISTER_STORE
+
+
+async def register_product_store(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data['new_product_store'] = update.message.text.upper()
+    data = '\n'.join(
+        [f'{key} - {value}' for key, value in context.user_data.items()
+         if key in ['new_product_name', 'new_product_price', 'new_product_store']]
+    )
+    del context.user_data['new_product_name']
+    del context.user_data['new_product_price']
+    del context.user_data['new_product_store']
+    await update.message.reply_text(f'Guardado!\n{data}')
+    return ConversationHandler.END
 
 
 async def search_products(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(update.effective_chat.id, '*Introduce el producto a buscar...(WIP)*', parse_mode=ParseMode.MARKDOWN)
+
+
+async def end_conversation_to_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await menu(update, context)
+    return ConversationHandler.END
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import logging
 import os
 
 import models
+import conversation_states
 from db import Base, engine
-from handlers import list_products, menu, register_products, search_products, start, help
+from handlers import end_conversation_to_menu, list_products, menu, register_product_name, register_product_price, register_product_store, register_products, search_products, start, help
 
 from dotenv import load_dotenv
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, ConversationHandler, MessageHandler, filters
 
 load_dotenv()
 
@@ -25,8 +26,25 @@ if __name__ == '__main__':
     application.add_handler(start_handler)
     application.add_handler(CommandHandler('menu', menu))
     application.add_handler(CommandHandler('help', help))
+    application.add_handler(ConversationHandler(
+        entry_points=[CommandHandler('registrar', register_products)],
+        states={
+            conversation_states.REGISTER_NAME: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND,
+                               register_product_name)
+            ],
+            conversation_states.REGISTER_PRICE: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND,
+                               register_product_price)
+            ],
+            conversation_states.REGISTER_STORE: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND,
+                               register_product_store)
+            ]
+        },
+        fallbacks=[CommandHandler("cancel", end_conversation_to_menu)]
+    ))
     application.add_handler(CommandHandler('listar', list_products))
-    application.add_handler(CommandHandler('registrar', register_products))
     application.add_handler(CommandHandler('buscar', search_products))
 
     application.run_polling()
